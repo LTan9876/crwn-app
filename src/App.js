6232f519270f4e043db4b27ab92e73987f4977c0
Lily Tan <lilyt93@gmail.com>
2020-08-20 16:35:52 -0400

    created redux user type file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user.actions'
@@ -52,15 +52,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path = '/' component = {HomePage} />
           <Route exact path = '/shop' component = {ShopPage} />
-          <Route exact path = '/signin' component = {SignInAndSignOutPage} />
+          <Route exact path = '/signin' render = {() => this.props.currentUser ? (<Redirect to= '/' />) : (<SignInAndSignOutPage/>)} />
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/redux/user/user.types.js
@@ -0,0 +1,5 @@
+//file is to prevent any spelling typos, imported into user.actions and users.reducer
+
+export const UserActionTypes = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
