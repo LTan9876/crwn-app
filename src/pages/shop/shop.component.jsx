bff13bd79cec4e01351caee7b85ddf24c65daff0
Lily Tan <lilyt93@gmail.com>
2020-09-28 13:38:14 -0400

    changed category name to collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import { Route } from 'react-router-dom'
-import CategoryPage from '../category/category.component'
+import CollectionPage from '../collection/collection.component'
 
 const ShopPage = ({ match }) => (
       <div className = 'shop-page'>
         {/* match is from history, want to display path current on */}
         <Route exact path = {`${match.path}`} component = {CollectionsOverview} />
-        {/* putting in the category ID as a param in path */}
-        <Route path = {`${match.path}/:categoryId`} component = {CategoryPage}/>
+        {/* putting in the collection ID as a param in path */}
+        <Route path = {`${match.path}/:collectionId`} component = {CollectionPage}/>
       </div>
 )
 
