6173e85cb6e56c73f299268c1816b0996546b469
Lily Tan <lilyt93@gmail.com>
2020-09-28 13:33:03 -0400

    created category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import { Route } from 'react-router-dom'
+import CategoryPage from '../category/category.component'
 
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
       <div className = 'shop-page'>
-        <CollectionsOverview />
+        {/* match is from history, want to display path current on */}
+        <Route exact path = {`${match.path}`} component = {CollectionsOverview} />
+        {/* putting in the category ID as a param in path */}
+        <Route path = {`${match.path}/:categoryId`} component = {CategoryPage}/>
       </div>
 )
 
