d837c2a35a4878087537c95918ca3911c8a1588a
Lily Tan <lilyt93@gmail.com>
2020-09-30 17:06:48 -0400

    updated selectors to be able to use obj data structure

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,12 @@ export const selectCollections = createSelector (
     shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollection],
+    //return value at that key
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
 //data normalization, data stored in obj vs array
 //changed shop/shop.data to obj instead of array so can do instant lookup, and not use find method to loop through entire array
 export const selectCollection = (collectionUrlParam) => 
