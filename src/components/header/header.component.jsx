61bab93356dddccb7d420f8d16c997c2996858cc
Lily Tan <lilyt93@gmail.com>
2020-10-02 16:13:07 -0400

    changed header component styles to styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,29 +9,30 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { createStructureSelector } from 'reselect'
 import { selectCartHidden } from '../../redux/cart/cart.selector'
 import { selectCurrentUser } from '../../redux/user/user.selector'
+import { HeaderContainer, LogoContainer, OptionDiv, OptionLink, OptionsContainer} from './header.styles'
 
 const Header = ({ currentUser, hidden }) => (
-  <div className = 'header'>
-    <Link className = 'logo-container' to = '/'>
+  <HeaderContainer>
+    <LogoContainer to = '/'>
       <Logo className = 'logo'/>
-    </Link>
-    <div className = 'options'>
-      <Link className = 'option' to = '/shop'> Shop</Link>
-      <Link className = 'option' to = '/shop'> Contact</Link>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to = '/shop'> Shop</OptionLink>
+      <OptionLink to = '/shop'> Contact</OptionLink>
       {/* ternary to decide to render sign in or sign out */}
       {/* sign out is taken care of by firebase */}
       {
         currentUser ?
-        <div className = 'option' onClick = {() => auth.signOut()}>Sign Out</div>
+        <OptionDiv onClick = {() => auth.signOut()}>Sign Out</OptionDiv>
         :
-        <Link className = 'option' to = '/signin'> Sign In</Link>
+        <OptionLink to = '/signin'> Sign In</OptionLink>
       }
       <CartIcon/>
-    </div>
+    </OptionsContainer>
     {
       hidden ? null : <CartDropdown/>
     }
-  </div>
+  </HeaderContainer>
 )
 
 
