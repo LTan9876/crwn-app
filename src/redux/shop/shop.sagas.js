62be16326d4b365004f67c20233262415e500cdf
Lily Tan <lilyt93@gmail.com>
2020-10-21 13:12:41 -0400

    moved redux shop action fetchCollectionsStartAsync over to redux shop saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,26 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, call, put } from 'redux-saga/effects'
 import ShopActionTypes from './shop.types'
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
 
 //sagas want to run concurrently without blocking each other
 export function* fetchCollectionsAsync() {
-    yield console.log('!')
+    //put is the saga effect for creating actions
+    //put can act like dispatch, have to use it with yield keyword
+    try {
+        const collectionRef = firestore.collection('collections')
+        const snapshot = yield collectionRef.get()
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+        yield put (fetchCollectionsSuccess(collectionsMap))
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message))
+    }
+
+        // collectionRef.get().then(snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+        //     updateCollections(collectionsMap)
+        //   }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+
 }
 
 //pauses when action types are in
