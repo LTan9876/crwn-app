930c2a2f9b190c820dc78b73cd98cbbae76b691f
Lily Tan <lilyt93@gmail.com>
2020-11-02 10:37:32 -0500

    added and commented out how useEffect could be used as componentWillUnmount

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './collection.styles.scss'
 import CollectionItem from '../../components/collection-item/collection-item.component'
 import { selectCollection } from '../../redux/shop/shop.selectors'
 import { connect } from 'react-redux'
+import { firestore } from '../../firebase/firebase.utils'
 
 const CollectionPage = ({ collection }) => {
+    // useEffect(() => {
+    //     const unsubscribeFromCollections = firestore.collection('collections').onSnapshot(async snapshot => console.log(snapshot))
+    //     return () => {
+    //         unsubscribeFromCollections()
+    //     }
+    // }, [])
+
     const { title, items } = collection
     return (
     <div className = 'collection-page'>
