c2733a034fce023a7c5dc79699ae0ecae6c32f9b
Lily Tan <lilyt93@gmail.com>
2020-11-03 14:39:25 -0500

    created server side code, incorporated stripe test payments

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express()
+const port = process.env.PORT || 5000
+
+//takes whatever is in body tag and outputs as json
+app.use(bodyParser.json())
+//makes sure there are no empty spaces or things urls don't like
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//cors = cross origin request
+//allows localhost 3000 to talk to localhost 5000
+app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+}
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+app.listen(port, error => {
+    if (error) throw error
+    console.log('server running on port' + port)
+})
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    }
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({ error: stripeErr })
+        } else {
+            res.status(200).send({ success: stripeRes })
+        }
+    })
+})
